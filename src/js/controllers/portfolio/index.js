faada639b118463bda2aca0d850174e35bebd82d
Connie Cho <hj.c.cho@gmail.com>
2016-08-09 00:36:20 -0400

    uncomment directives in portfolio/index.js

--- src/js/controllers/portfolio/index.js
+++ src/js/controllers/portfolio/index.js
@@ -10,43 +10,43 @@ require('./../../../css/style.css');
 
 
 angular.module('portfolio', [animate]).controller('PortfolioController', require('./portfolio.js'))
-// .run(function ($anchorScroll, $window) {
-//   var wWidth = $(window).width(); // get the width of the window
-//   var topoffset = 0;
-//   if (wWidth < 768) {
-//     topoffset = 56;
-//   } else if (wWidth >= 768 & wWidth <= 991) {
-//     topoffset = 135;
-//   } else {
-//     // when window width is greater than 992
-//     topoffset = 85;
-//   }
-//   $anchorScroll.yOffset = topoffset-2;
-// })
-// .directive('scrollOnClick', function() {
-//   return {
-//     restrict: 'A',
-//     link: function($scope, element, attrs) {
-//       $scope.scrollTo = function(id) {
-//         $anchorScroll(id);
-//       }
-//     }
-//   }
-// })
-// .directive('scrollFade', function($window) {
-//   return {
-//     scope: false,
-//     link: function($scope, element, attrs) {
-//       angular.element($window).bind("scroll", function() {
-//         if (this.pageYOffset >= 100) {
-//           $scope.changeBackground = true;
-//          } else {
-//            $scope.changeBackground = false;
-//          }
-//         $scope.$apply();
-//       })
-//     }
-//   }
-// });
+.run(function ($anchorScroll, $window) {
+  var wWidth = $(window).width(); // get the width of the window
+  var topoffset = 0;
+  if (wWidth < 768) {
+    topoffset = 56;
+  } else if (wWidth >= 768 & wWidth <= 991) {
+    topoffset = 135;
+  } else {
+    // when window width is greater than 992
+    topoffset = 85;
+  }
+  $anchorScroll.yOffset = topoffset-2;
+})
+.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function($scope, element, attrs) {
+      $scope.scrollTo = function(id) {
+        $anchorScroll(id);
+      }
+    }
+  }
+})
+.directive('scrollFade', function($window) {
+  return {
+    scope: false,
+    link: function($scope, element, attrs) {
+      angular.element($window).bind("scroll", function() {
+        if (this.pageYOffset >= 100) {
+          $scope.changeBackground = true;
+         } else {
+           $scope.changeBackground = false;
+         }
+        $scope.$apply();
+      })
+    }
+  }
+});
 
 module.exports = 'portfolio';
