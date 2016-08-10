ef722f8cab8ed9a0037d74f300974a99b9ed1353
plaeski <lisa.plaseski@gmail.com>
2016-08-09 23:06:44 -0400

    Updated webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
  * Env
  * Get npm lifecycle event to identify the environment
  */
-var ENV = process.env.npm_lifecycle_event;
-var isTest = ENV === 'test' || ENV === 'test-watch';
-var isProd = ENV === 'build';
+var ENV = 'build';
+// var isTest = ENV === 'test' || ENV === 'test-watch';
+// var isProd = ENV === 'build';
 
 module.exports = function makeWebpackConfig () {
   /**
